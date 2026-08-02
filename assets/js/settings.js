4d13058cb7a9e4d788d93efee5d1655824d0b2ab
Alfiz Ilham <alfizilham51@outlook.com>
2026-08-02 14:55:26 +0700

    feat: course notes view mode persisten setelah Simpan + tombol Edit

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -233,7 +233,7 @@ function deleteAccount() {
 
         var keys = ['course_completion', 'custom_courses', 'archived_courses', 'certificates',
             'study_completion', 'study_log', 'study_minggu_terakhir', 'custom_subjects', 'study_edits',
-            'todos', 'daily_tasks', 'finance_records', 'settings_profile'
+            'todos', 'daily_tasks', 'finance_records', 'settings_profile', 'course_notes_view'
         ];
         keys.forEach(function (k) { localStorage.removeItem(k); });
 
@@ -248,7 +248,7 @@ function logoutDemo() {
             var keys = ['course_completion', 'custom_courses', 'archived_courses',
                 'certificates', 'study_completion', 'study_log', 'study_minggu_terakhir',
                 'custom_subjects', 'study_edits', 'todos', 'daily_tasks', 'finance_records',
-                'settings_profile', 'course_notes', 'course_edits'
+                'settings_profile', 'course_notes', 'course_edits', 'course_notes_view'
             ];
             keys.forEach(function (k) { localStorage.removeItem(k); });
             clearAuthSession();
